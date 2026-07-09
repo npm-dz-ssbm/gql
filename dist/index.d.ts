fd4f1e3be4e2cd50e483a9c04c30866f49ed1808
Mitch Dzugan <mitchdzugan@gmail.com>
2026-07-09 05:22:07 -0500

    prefer using prepare

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as $ from "@dz-ssbm/util";
-import { type Client, type ClientOpts, type Operation, type ResType, type VarsType } from "./T.js";
-export declare function Client(url: string, clientOpts?: ClientOpts): Client;
-export declare function Operation<VT extends VarsType, RT extends ResType>(query: string, vt: $.T.ZodType<VT>, rt: $.T.ZodType<RT>): Operation<VT, RT>;
-export * from "./T.js";
-//# sourceMappingURL=index.d.ts.map
--- src/T.ts
+++ src/T.ts
@@ -2,58 +2,35 @@ import * as $ from "@dz-ssbm/util";
 
 export type NetworkControl = "use-cache" | "cache-only" | "force-fetch";
 export const NetworkControl = {
-    useCache: "use-cache" as NetworkControl,
-    cacheOnly: "cache-only" as NetworkControl,
-    forceFetch: "force-fetch" as NetworkControl,
+  useCache: "use-cache" as NetworkControl,
+  cacheOnly: "cache-only" as NetworkControl,
+  forceFetch: "force-fetch" as NetworkControl,
 };
 
 export type VarsType = Record<string, string | number | boolean | null>;
 export type ResType = Record<string, unknown>;
 export type Opts = {
-    log?: (...s: string[]) => void;
-    networkControl?: NetworkControl | undefined;
-    rawQueryStr?: string | undefined;
-    cachePath?: string | undefined;
+  log?: (...s: string[]) => void;
+  networkControl?: NetworkControl | undefined;
+  rawQueryStr?: string | undefined;
+  cachePath?: string | undefined;
 };
 export function Opts(opts: Opts): Opts {
-    return opts;
+  return opts;
 }
 
 export const Error: $.T.VariantDef<
-    "@dz-ssbm/gql|Error",
-    {
-        NetworkError: $.T.ZodUnknown;
-        CachePrepError: $.T.ZodUnknown;
-        CacheWriteError: $.T.ZodUnknown;
-        CacheOnlyEmpty: $.T.ZodUndefined;
-    }
+  "@dz-ssbm/gql|Error",
+  {
+    NetworkError: $.T.ZodUnknown;
+    CachePrepError: $.T.ZodUnknown;
+    CacheWriteError: $.T.ZodUnknown;
+    CacheOnlyEmpty: $.T.ZodUndefined;
+  }
 > = $.T.defVariant("@dz-ssbm/gql|Error", {
-    NetworkError: $.T.unknown(),
-    CachePrepError: $.T.unknown(),
-    CacheWriteError: $.T.unknown(),
-    CacheOnlyEmpty: $.T.undefined(),
+  NetworkError: $.T.unknown(),
+  CachePrepError: $.T.unknown(),
+  CacheWriteError: $.T.unknown(),
+  CacheOnlyEmpty: $.T.undefined(),
 });
 export type Error = $.T.inferDefined<typeof Error>;
-
-export type Client = {
-    url: string;
-    authToken?: string | undefined;
-    baseOpts: Opts;
-    operate<VT extends VarsType, RT extends ResType>(
-        op: Operation<VT, RT>,
-        vars: VT,
-        opts?: Opts,
-    ): $.Xa<RT, Error>;
-    operateUnknown(
-        op: string,
-        vars?: VarsType,
-        opts?: Opts,
-    ): $.Xa<ResType, Error>;
-};
-
-export type ClientOpts = Opts & { authToken?: string };
-export type Operation<VT extends VarsType, RT extends ResType> = {
-    query: string;
-    vt: $.T.ZodType<VT>;
-    rt: $.T.ZodType<RT>;
-};
